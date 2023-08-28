02f85d33df7570079087db1c0bc91d85faf3463e
Clayton Hickey <clayton@claytondoesthings.xyz>
2023-08-28 14:13:14 -0400

    added nixpkg

---
+++ default.nix
@@ -0,0 +1,42 @@
+{
+    #rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+    pkgs ? import <nixpkgs> {
+        overlays = [
+            (import (fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"))
+        ];
+    },
+}:
+let
+    rustPlatform = pkgs.makeRustPlatform {
+        cargo = pkgs.rust-bin.nightly."2023-03-15".minimal;
+        rustc = pkgs.rust-bin.nightly."2023-03-15".minimal;
+    };
+
+    builder = rustPlatform.buildRustPackage {
+        pname = "claytonhickeyme-builder";
+        version = "af167b803b4f58cb2b92415ff1f4ca5d7f33b73e";
+        src = ./.;
+
+        cargoLock = {
+            lockFile = ./Cargo.lock;
+            outputHashes = {
+                "chrono-0.4.20-beta.1" = "sha256-7LlcYbWbO8Up3iHXRmtmIKMZmVG0ebB+q6+gM1fuA7A=";
+            };
+        };
+
+        doCheck = false;
+
+        meta = {
+            description = "builds claytonhickey.me";
+        };
+    };
+in
+pkgs.stdenv.mkDerivation {
+    pname = "claytonhickeyme";
+    version = "af167b803b4f58cb2b92415ff1f4ca5d7f33b73e";
+    src = ./.;
+
+    buildPhase = ''
+        echo "y" | DOMAIN="https://claytonhickey.me" WWW_DIR="$out" ${builder}/bin/claytonhickey_me
+    '';
+}
