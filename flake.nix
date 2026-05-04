ec167dfedd4204e237f0dfec16437af1bde16e90
clay53 <clayton@claytondoesthings.xyz>
2026-05-03 23:30:43 -0400

    add nix derivation

--- flake.nix
+++ flake.nix
@@ -24,5 +24,31 @@
 
       TOPFIND = "${pkgs.ocamlPackages.findlib}/lib/ocaml/*/site-lib/topfind";
     };
+
+    packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
+      name = "claytonhickey_me";
+      version = "1.0.0";
+      src = ./.;
+
+      buildInputs = with pkgs; [
+        dune_3
+        ocaml
+        ocamlPackages.findlib
+        ocamlPackages.lambdasoup
+      ];
+
+      TOPFIND = "${pkgs.ocamlPackages.findlib}/lib/ocaml/*/site-lib/topfind";
+
+      buildPhase = ''
+        mkdir $out
+        cp -r $src $out/build
+        chmod -R +w $out/build
+        cd $out/build
+        dune build
+        ocaml no_dune.ml
+        mv $out/build/www $out/www
+        rm -r $out/build
+      '';
+    };
   };
 }
